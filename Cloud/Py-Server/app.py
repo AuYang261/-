83cf3a648de514238a14c2548468eda7be24a685
unknown <459461160@qq.com>
2022-04-30 21:36:27 +0800

    删除了登录系统；增加了前端文件上传的demo和后端接收的接口，在test.html里

--- Cloud/Py-Server/app.py
+++ Cloud/Py-Server/app.py
@@ -18,26 +18,39 @@ def index():
     # 查询数据
     results = models.User.query.all()
     print(results, results[0].username if results else "empty!")
-    
-    if 'username' in session:
-        res = make_response(render_template('welcome.html', method=request.method, data=request.data.decode()))
-        res.set_cookie('username', 'the username')
-        return res
-    else:
-        app.logger.error("user not login")
-        return redirect(url_for('login'))
+
+    # 这是登录系统要考虑的
+    # username = request.form.get('username')
+    # if username not in session:
+    #     app.logger.error("user not login")
+    #     return redirect(url_for('login'))
+
+    res = make_response(render_template('welcome.html', method=request.method, data=request.data.decode()))
+    return res
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         app.logger.debug("user login")
 
-        # 增加数据
+        # 向数据库中增加数据
         user1 = models.User(username = request.form['username'])
         db.session.add_all([user1])
         db.session.commit()
 
-        session['username'] = request.form['username']
+        username = request.form['username']
+        session[username] = username
         return redirect(url_for('index'))
-    else:
+    else: #返回登录页面
         return render_template('login.html')
+
+@app.route("/uploadFile", methods=['POST'])
+def uploadFile():
+    print(request)
+    return make_response(render_template('welcome.html', method=request.method, data=request.data.decode()))
+    pass
+
+@app.route("./edgeServer", methods=['GET'])
+def edgeServer():
+    return '127.0.0.1'
+    pass
